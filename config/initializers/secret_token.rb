34dc5f4aa589121e2381e7a2b5fa7f0a8b08123f
taoromera <taoromera@gmail.com>
2012-10-09 10:51:45 +0900

    Added secret_token.rb

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sanpo::Application.config.secret_token = '20d0d2b08537d798fa415022f98dee91580bff8b179ea373738c2e9fbb4abbcc4d52b207e4c5c82a3c0f6a1366b1e0ba28c245d023e06fa2776d1c2c96d45aff'
