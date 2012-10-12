ac8039173aa34b200c2bffaaa2cb4824930cd822
taoromera <taoromera@gmail.com>
2012-10-12 21:23:44 +0900

    Changed tablee creation script

--- Preprocessing/Table_init.sql
+++ Preprocessing/Table_init.sql
@@ -3,7 +3,7 @@ CREATE SEQUENCE sanpo_routes_id;
 CREATE TABLE IF NOT EXISTS sanpo_routes (
   id bigint NOT NULL DEFAULT nextval('sanpo_routes_id'),
   user_route_id bigint NOT NULL,
-  user_id varchar(32) NOT NULL,
+  user_id text NOT NULL,
   start_time timestamp NOT NULL,
   end_time timestamp NOT NULL,
   start_lat decimal(9,6) NOT NULL,
@@ -11,6 +11,8 @@ CREATE TABLE IF NOT EXISTS sanpo_routes (
   end_lat decimal(9,6) NOT NULL,
   end_lng decimal(9,6) NOT NULL,
   geom geometry(MultiLineString,4326),
+  length decimal(5,3),
+  tracking_times text,
   public boolean NOT NULL DEFAULT '1',
   PRIMARY KEY (id)
 );
@@ -19,7 +21,9 @@ CREATE SEQUENCE sanpo_photos_id;
 
 CREATE TABLE IF NOT EXISTS sanpo_photos (
   id bigint NOT NULL DEFAULT nextval('sanpo_photos_id'),
+  user_id text NOT NULL,
   user_route_id bigint NOT NULL,
+  user_photo_id bigint NOT NULL,
   lat decimal(9,6) NOT NULL,
   lng decimal(9,6) NOT NULL,
   shoot_time timestamp NOT NULL,
